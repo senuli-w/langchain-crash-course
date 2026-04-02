335fb5181c157d18d2b4e6d4eebf12d2bf93f460
Senuli Wickramage <senuli.20220950@iit.ac.lk>
2026-04-02 12:58:06 +0530

    refactor: streamline model invocation by removing static conversation and using streaming output

--- main.py
+++ main.py
@@ -11,14 +11,5 @@ model = init_chat_model(
     temperature=0.1,
 )
 
-conversation = [
-    SystemMessage("You are a helpful assistant that provides information about programming languages."),
-    HumanMessage("Hello, what is Python?"),
-    AIMessage("Python is a high-level, interpreted programming language known for its simplicity and readability. It is widely used for web development, data analysis, artificial intelligence, scientific computing, and more. Python has a large standard library and a vibrant community that contributes to its extensive ecosystem of third-party packages."),
-    HumanMessage("When was it released?"),
-]
-
-response = model.invoke(conversation)
-
-# print(response)
-print(response.content)
+for chunk in model.stream("What is Python?"):
+    print(chunk.text, end="", flush=True)
