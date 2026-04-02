2c7e9b2160873de90dea23af6b8fa58af63128ed
Senuli Wickramage <senuli.20220950@iit.ac.lk>
2026-04-02 12:55:14 +0530

    refactor: restructure model invocation to use a conversation list for improved context

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from dotenv import load_dotenv
 
 from langchain.chat_models import init_chat_model
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
@@ -10,7 +11,14 @@ model = init_chat_model(
     temperature=0.1,
 )
 
-response = model.invoke("Hello, what is Python?")
+conversation = [
+    SystemMessage("You are a helpful assistant that provides information about programming languages."),
+    HumanMessage("Hello, what is Python?"),
+    AIMessage("Python is a high-level, interpreted programming language known for its simplicity and readability. It is widely used for web development, data analysis, artificial intelligence, scientific computing, and more. Python has a large standard library and a vibrant community that contributes to its extensive ecosystem of third-party packages."),
+    HumanMessage("When was it released?"),
+]
+
+response = model.invoke(conversation)
 
 # print(response)
 print(response.content)
